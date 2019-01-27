36a38f5050842f576e5ffc3cefd682a048f00288
Cristi <cristi_zmf@yahoo.com>
2019-01-27 15:56:38 +0200

    first commit

---
+++ src/main/java/com/cristi/mentool/mentoolfront/MentoolFrontApplication.java
@@ -0,0 +1,14 @@
+package com.cristi.mentool.mentoolfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoolFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoolFrontApplication.class, args);
+	}
+
+}
+

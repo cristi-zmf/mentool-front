dce3e1d118619dd0e059f9424a800e609df1823b
Cristi <cristi_zmf@yahoo.com>
2019-02-11 23:56:53 +0200

    resource for adding new mentor

--- src/main/java/com/cristi/mentool/mentoolfront/exposition/registration/mentor/MentorRegistrationResource.java
+++ src/main/java/com/cristi/mentool/mentoolfront/exposition/registration/mentor/MentorRegistrationResource.java
@@ -1,28 +1,32 @@
-package com.cristi.mentool.mentoolfront.exposition.registration.user;
+package com.cristi.mentool.mentoolfront.exposition.registration.mentor;
 
 import com.cristi.mentool.mentoolfront.domain.Role;
 import com.cristi.mentool.mentoolfront.domain.security.AddNewAuthority;
 import com.cristi.mentool.mentoolfront.exposition.MentoolRequestMapping;
+import com.cristi.mentool.mentoolfront.exposition.registration.user.UserCreateCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @MentoolRequestMapping
-public class UserRegistrationResource {
+public class MentorRegistrationResource {
     private final AddNewAuthority addNewAuthority;
     private final RestTemplate restTemplate;
 
     private final static String PERSONS_SERVICE = "http://persons";
 
 
-    public UserRegistrationResource(
+    public MentorRegistrationResource(
             AddNewAuthority addNewAuthority,
             @Autowired @LoadBalanced RestTemplate restTemplate
     ) {
@@ -30,13 +34,14 @@ public class UserRegistrationResource {
         this.restTemplate = restTemplate;
     }
 
-    @PostMapping(value = "/users")
-    public String registerUser(@Valid @RequestBody UserCreateCommand registerCommand) throws AuthenticationException {
-        registerCommand.role = Role.USER;
+    @PutMapping(value = "/mentors")
+    public ResponseEntity<String> registerMentor(@Valid @RequestBody MentorRegistrationCommand registerCommand) throws AuthenticationException {
+        registerCommand.role = Role.MENTOR;
         addNewAuthority.addAuthorityFor(registerCommand);
-        return restTemplate.exchange(
-                PERSONS_SERVICE + "/persons/users", HttpMethod.POST,
+        String responseBody = restTemplate.exchange(
+                PERSONS_SERVICE + "/persons/mentors", HttpMethod.PUT,
                 new HttpEntity<>(registerCommand), String.class
         ).getBody();
+        return ok(responseBody);
     }
 }

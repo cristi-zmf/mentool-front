a28a83a53fd425f50f1287961f2b8f6102edbbcb
cristi-zmf <cristi_zmf@yahoo.com>
2019-02-12 11:06:11 +0200

    improved mentor form

--- src/main/resources/mentool-ui/src/app/mentor/mentor.component.ts
+++ src/main/resources/mentool-ui/src/app/mentor/mentor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-mentor',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentor.component.css']
 })
 export class MentorComponent implements OnInit {
-
-  constructor() { }
+  private mentorForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.email]),
+    password: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    yearsOfExperience: new FormControl(''),
+    linkedinUrl: new FormControl('')
+  });
+  constructor(private toastrService: ToastrService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.mentorForm.value);
+    this.toastrService.success(this.mentorForm.value, "Form status");
+  }
 }
